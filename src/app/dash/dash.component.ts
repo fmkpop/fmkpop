11d8b33a70359b6b14be6ef76dbb283e2549415f
fmkpop <87940106+fmkpop@users.noreply.github.com>
2022-02-15 19:40:27 +0000

    Update dash.component.ts

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -1,22 +1,26 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Observable, forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators';
 import data from '../../assets/id_girls.json'
-import { Girl, RedditJson, Card } from '../model';
+import { Girl, RedditJson, Card, GirlVote, VoteData } from '../model';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-dash',
   templateUrl: './dash.component.html',
   styleUrls: ['./dash.component.css']
 })
-export class DashComponent {
+export class DashComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
+  url = `https://getpantry.cloud/apiv1/pantry/b79d34bf-9370-43fc-b088-d2ba6e5588e6/basket/girls`
   buttonStates: string[] = ['', '', '']
   girls: Girl[] = []
-
+  localVotes = []
+  data!: VoteData[]
+  showNextButton = false
   cards: Observable<Card[]> = forkJoin([{}, {}, {}].map(_ => {
     const girl = this.randomGirl()
     this.girls.push(girl)
@@ -24,6 +28,11 @@ export class DashComponent {
       return { rows: 1, cols: 1, title: girl.name + " - " + girl.group, url: imageUrl } as Card
     }))
   }))
+
+  ngOnInit() {
+    this.readNetworkStorage().subscribe(data => this.data = data)
+    this.readLocalStorage()
+  }
 
   uniqueSelections = () => JSON.stringify([...this.buttonStates].sort()) == "[\"f\",\"k\",\"m\"]"
 
@@ -44,8 +53,45 @@ export class DashComponent {
     const time = new Date().toISOString()
     const girlVotes = this.girls.map((g, i) => { return { ...g, vote: this.buttonStates[i] } })
     const body = { [time]: girlVotes }
-    const url = `https://getpantry.cloud/apiv1/pantry/b79d34bf-9370-43fc-b088-d2ba6e5588e6/basket/girls`
-    this.http.put(url, body).subscribe(success => window.location.reload(), error => window.location.reload())
+    this.setLocalStorage(JSON.stringify(body))
+    this.setNetworkStorage(body)
+    this.alertData()
+  }
+
+  setNetworkStorage(payload: {}) {
+    this.http.put(this.url, payload).subscribe(success => window.location.reload(), error => window.location.reload())
+  }
+
+  setLocalStorage(newVotes: string) {
+    const curr = localStorage.getItem("fmkpop")
+    const totalVotes = curr ? curr + ', ' + newVotes : newVotes
+    console.log("ls", totalVotes)
+    localStorage.setItem("fmkpop", totalVotes)
+  }
+
+  readLocalStorage() {
+    const data = JSON.parse('[' + localStorage.getItem('fmkpop') + ']')
+    this.localVotes = data.map((x: any) => Object.values(x)[0])
+  }
+
+  readNetworkStorage(): Observable<VoteData[]> {
+    const fmk = (xs: string[], a: string) => xs.filter(l => l == a).length
+
+    return this.http.get(this.url).pipe(map(data => {
+      const votes: GirlVote[] = _.flatten(Object.values(data))
+      const girlData = this.girls.map(g => votes.filter(v => g.id == v.id))
+      const scores = girlData.map(g => g.map(v => v.vote))
+      const numbers = scores.map((s, i) => {
+        return { g: this.girls[i].name, f: fmk(s, 'f'), m: fmk(s, 'm'), k: fmk(s, 'k') }
+      })
+      console.log(numbers)
+      return numbers
+    }))
+  }
+
+  alertData() {
+    const str = this.data.map(d => `${d.g} - f: ${d.f}, m: ${d.m}, k: ${d.k}`).join('\n')
+    alert(str)
   }
 
   randId(): number {
@@ -55,7 +101,6 @@ export class DashComponent {
 
   randomGirl(): Girl {
     const girl = data.find(girl => girl.id === this.randId()) || this.randomGirl()
-    console.log(girl)
     return girl
   }
 
